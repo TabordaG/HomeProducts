04326aeb49f7133df852b0b3ea403c92eb9d23d6
Garrike <43554965+Garrike@users.noreply.github.com>
2020-04-30 23:34:41 -0400

    Trying dragg list

--- lib/main.dart
+++ lib/main.dart
@@ -33,122 +33,160 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Scaffold(
         body: Stack(
           children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(left: 20, top: 10, right: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      SvgPicture.asset("assets/icons/menu.svg", color: Color.fromRGBO(76, 150, 203, 1),),
-                      Image.asset("assets/images/user.png"),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Text("Hey Gabriel,", style: kHeadingextStyle),
-                  Text("Find a product you want to taste",
-                      style: kSubheadingextStyle),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 15),
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    height: 60,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Color(0xFFF5F5F7),
-                      borderRadius: BorderRadius.circular(40),
-                    ),
-                    child: Row(
-                      children: <Widget>[
-                        SvgPicture.asset("assets/icons/search.svg", color: Color.fromRGBO(76, 150, 203, 1),),
-                        SizedBox(width: 10),
-                        Text(
-                          "Search for anything",
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Color(0xFFA0A5BD),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text("Category", style: kTitleTextStyle),
-                      Text(
-                        "See All",
-                        style: kSubtitleTextSyule.copyWith(color: Color.fromRGBO(76, 150, 203, 1)),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 15),
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(40.0),
-                  topLeft: Radius.circular(40.0),
-                ),
-              ),
-              child: DraggableScrollableSheet(
-                initialChildSize: initialSize,
-                minChildSize: initialSize,
-                maxChildSize: 0.8,
-                builder: (context, scrollController) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(40.0),
-                        topLeft: Radius.circular(40.0),
-                      ),
-                      color: Color.fromRGBO(76, 150, 203, 0.9),
-                    ),
-                    padding: EdgeInsets.only(top: 20, right: 10, left: 10),
-                    child: StaggeredGridView.countBuilder(                      
-                      controller: scrollController, //SliverStaggeredGridDelegateWithFixedCrossAxisCount usar com CustomScrollView
-                      crossAxisCount: 2,
-                      itemCount: categories.length,
-                      crossAxisSpacing: 20,
-                      mainAxisSpacing: 20,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          padding: EdgeInsets.all(20),
-                          height: index.isEven ? 200 : 240,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(16),
-                            image: DecorationImage(
-                              image: AssetImage(categories[index].image),
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                categories[index].name,
-                                style: kTitleTextStyle,
-                              ),
-                              Text(
-                                '${categories[index].numOfCourses} Courses',
-                                style: TextStyle(
-                                  color: kTextColor.withOpacity(.5),
-                                ),
-                              )
-                            ],
-                          ),
-                        );
-                      },
-                      staggeredTileBuilder: (index) => StaggeredTile.fit(1),
-                    ),
-                  );
-                }
-              ),
-            ),
+            TopPageHome(),
+            DraggableScroll(initialSize: initialSize),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class DraggableScroll extends StatelessWidget {
+  const DraggableScroll({
+    Key key,
+    @required this.initialSize,
+  }) : super(key: key);
+
+  final double initialSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(40.0),
+          topLeft: Radius.circular(40.0),
+        ),
+      ),
+      child: DraggableScrollableSheet(
+        initialChildSize: initialSize,
+        minChildSize: initialSize,
+        maxChildSize: 0.9,
+        builder: (context, scrollController) {
+          return Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(40.0),
+                topLeft: Radius.circular(40.0),
+              ),
+              color: Color.fromRGBO(39, 113, 177, 1),
+            ),
+            padding: EdgeInsets.only(top: 20, right: 10, left: 10),
+            child: ListView(
+              controller: scrollController,
+              children: <Widget>[
+                Center(child: Text('Teste ListView')),
+                SizedBox(height: 20,),
+                Container(
+                  width: double.infinity,
+                  // height: 200,
+                  height: initialSize * MediaQuery.of(context).size.height,
+                  child: StaggeredGridView.countBuilder(                      
+                    // controller: scrollController, //SliverStaggeredGridDelegateWithFixedCrossAxisCount usar com CustomScrollView
+                    crossAxisCount: 2,
+                    shrinkWrap: true,
+                    itemCount: categories.length,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        padding: EdgeInsets.all(20),
+                        height: index.isEven ? 200 : 240,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          image: DecorationImage(
+                            image: AssetImage(categories[index].image),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              categories[index].name,
+                              style: kTitleTextStyle,
+                            ),
+                            Text(
+                              '${categories[index].numOfCourses} Courses',
+                              style: TextStyle(
+                                color: kTextColor.withOpacity(.5),
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                    staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+      ),
+    );
+  }
+}
+
+class TopPageHome extends StatelessWidget {
+  const TopPageHome({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 20, top: 10, right: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              SvgPicture.asset("assets/icons/menu.svg", color: Color.fromRGBO(76, 150, 203, 1),),
+              Image.asset("assets/images/user.png"),
+            ],
+          ),
+          SizedBox(height: 20),
+          Text("Hey Gabriel,", style: kHeadingextStyle),
+          Text("Find a product you want to taste",
+              style: kSubheadingextStyle),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 15),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            height: 60,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Color(0xFFF5F5F7),
+              borderRadius: BorderRadius.circular(40),
+            ),
+            child: Row(
+              children: <Widget>[
+                SvgPicture.asset("assets/icons/search.svg", color: Color.fromRGBO(76, 150, 203, 1),),
+                SizedBox(width: 10),
+                Text(
+                  "Search for anything",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Color(0xFFA0A5BD),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text("Category", style: kTitleTextStyle),
+              Text(
+                "See All",
+                style: kSubtitleTextSyule.copyWith(color: Color.fromRGBO(76, 150, 203, 1)),
+              ),
+            ],
+          ),
+          SizedBox(height: 15),
+        ],
       ),
     );
   }
